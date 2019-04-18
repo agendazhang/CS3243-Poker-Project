1f8894a2b0bec35de83c9ba83be15d5e6f365933
amuletxheart <xinshen@u.nus.edu>
2019-04-18 14:54:31 +0800

    updated poker engine

--- example.py
+++ example.py
@@ -1,16 +1,16 @@
 from pypokerengine.api.game import setup_config, start_poker
 from randomplayer import RandomPlayer
 from raise_player import RaisedPlayer
-from smarterplayer import SmarterPlayer
-from smarterplayerimprovedpreflop import SmarterPlayerImprovedPreflop
+from smartestplayer import SmartestPlayer
+from smartestplayerimprovedpreflop import SmartestPlayerImprovedPreflop
 
 #TODO:config the config as our wish
-config = setup_config(max_round=1000, initial_stack=10000, small_blind_amount=10)
+config = setup_config(max_round=500, initial_stack=10000, small_blind_amount=10)
 
 
 
-config.register_player(name="f1", algorithm=SmarterPlayer())
-config.register_player(name="FT2", algorithm=SmarterPlayerImprovedPreflop())
+config.register_player(name="f1", algorithm=SmartestPlayer())
+config.register_player(name="FT2", algorithm=RaisedPlayer())
 
 
 game_result = start_poker(config, verbose=1)
