100de158f0c04bd5dd3dbac884af6363ae19c5cb
agendazhang <e0202838@u.nus.edu>
2019-04-18 21:17:22 +0800

    Change inner dictionary values to action probabilities

--- example.py
+++ example.py
@@ -12,13 +12,8 @@ config = setup_config(max_round=500, initial_stack=10000, small_blind_amount=10)
 
 
 
-<<<<<<< HEAD
-config.register_player(name="P1", algorithm=SmartestPlayer())
+config.register_player(name="p1", algorithm=SmartestPlayer())
 config.register_player(name="P2", algorithm=SmartestPlayerImprovedPreflop())
-=======
-config.register_player(name="f1", algorithm=Group40Player())
-config.register_player(name="FT2", algorithm=SmartestPlayerImprovedPreflop())
->>>>>>> 497c452bae5be41af1e332e162cc6a9715964a3e
 
 
 game_result = start_poker(config, verbose=1)
